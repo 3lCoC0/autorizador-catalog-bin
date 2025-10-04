166572cc89d0fae3bbd4011c2dd6b6b2622a0eaf
Camilo Cardenas <fcardenas61@gmail.com>
2025-10-04 14:12:54 -0500

    Se corrige el error en el app execption

--- src/main/java/com/credibanco/authorizer_catalog_bin_manager_cf/infrastructure/exception/AppException.java
+++ src/main/java/com/credibanco/authorizer_catalog_bin_manager_cf/infrastructure/exception/AppException.java
@@ -1,5 +1,9 @@
 package com.credibanco.authorizer_catalog_bin_manager_cf.infrastructure.exception;
 
+
+import lombok.Getter;
+
+@Getter
 public class AppException extends RuntimeException {
     private final AppError error;
 
